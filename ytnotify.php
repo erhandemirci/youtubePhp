5b2457ac3b621ed5303352e04a3a5d76b9fd3c52
Erhan <demirci.erhan@windowslive.com>
2022-04-17 13:14:58 +0300

    Update ytnotify.php

--- ytnotify.php
+++ ytnotify.php
@@ -143,8 +143,49 @@ if ($notify) {
     $data = json_encode(array(
         'content' => $msg
     ));
+    
+    
+    function sendMessage(){
+        $content = array(
+            "en" => 'English Message'
+            );
+        
+        $fields = array(
+            'app_id' => "5eb5a37e-b458-11e3-ac11-000c2940e62c",
+            'filters' => array(array("field" => "tag", "key" => "level", "relation" => "=", "value" => "10"),array("operator" => "OR"),array("field" => "amount_spent", "relation" => "=", "value" => "0")),
+            'data' => array("foo" => "bar"),
+            'contents' => $content
+        );
+        
+        $fields = json_encode($fields);
+        print("\nJSON sent:\n");
+        print($fields);
+        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8',
+                                                   'Authorization: Basic NGEwMGZmMjItY2NkNy0xMWUzLTk5ZDUtMDAwYzI5NDBlNjJj'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+        
+        return $response;
+    }
+    
+    $response = sendMessage();
+    $return["allresponses"] = $response;
+    $return = json_encode( $return);
+    
+    print("\n\nJSON received:\n");
+    print($return);
+    print("\n");
    
-  
+  /*
 
     // cURL away!
     $curl = curl_init();
@@ -162,7 +203,7 @@ if ($notify) {
 
     // Save latest date to file
     file_put_contents($LATEST_FILE, $inputdate);
-    
+    */
 }
 
 ?>
